535a611466fbbb6da29883cdb28524119a4b9458
likerang <arang9314@naver.com>
2025-07-18 17:10:05 +0900

    중첩 라우트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router" //주소표시줄 경로에 따라서 실행될 컴포넌트 
+import { Routes, Route, NavLink, useParams} from "react-router" //주소표시줄 경로에 따라서 실행될 컴포넌트 
+
+const contents = [
+  {id:1, title:'기획', desc:'웹 기획 !'},
+  {id:2, title:'디자인', desc:'웹 디자인 !'},
+  {id:3, title:'구현', desc:'웹 구현 !'},
+  {id:4, title:'취업', desc:'웹 분야 취업 !!!!!!!!!!!!!!!'}
+]
 
 const Contact = ()=>{
   return (
@@ -9,11 +16,35 @@ const Contact = ()=>{
     </div>
   );
 }
+const Task = ()=>{
+  let params = useParams();
+  console.log(typeof(params.task_id));
+  let data = contents.find(c=>c.id === Number(params.task_id));
+  console.log(data);
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      <p>{data.desc}</p>
+    </div>
+  );
+}
 const Tasks = ()=>{
+  let list = [];
+  contents.forEach(c=>{
+    list.push(
+    <li key={c.id}><NavLink to={`/Tasks/${c.id}`}>{c.title}</NavLink></li>
+  )
+  })
   return (
     <div>
       <h2>Tasks</h2>
-      <p>Tasks...</p>
+      <ul>
+        {list}
+      </ul>
+      <Routes>
+        <Route path=":task_id" element={<Task/>}/>
+      </Routes>
+      
     </div>
   );
 }
@@ -21,7 +52,8 @@ const Home = ()=>{
   return (
     <div>
       <h2>🏡Home</h2>
-      <p>Home...</p>
+      <p>My Sweet Home...</p>
+      <img src="/lisbon.jpg"/>
     </div>
   );
 }
@@ -31,14 +63,14 @@ const App = ()=>{
       <h1>React Router DOM</h1>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Tasks">Tasks</Link></li>
-          <li><Link to="/Contact">Contact</Link></li>
+          <li><NavLink  to="/">Home</NavLink></li>
+          <li><NavLink  to="/Tasks">Tasks</NavLink></li>
+          <li><NavLink  to="/Contact">Contact</NavLink></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/Tasks" element={<Tasks/>}/>
+        <Route path="/Tasks/*" element={<Tasks/>}/>
         <Route path="/Contact" element={<Contact/>}/>
       </Routes>
     </div>
