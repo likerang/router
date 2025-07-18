a70e78d113f04efc1514a8889ffda7eca97a4cb8
likerang <arang9314@naver.com>
2025-07-18 16:20:38 +0900

    link 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router" //주소표시줄 경로에 따라서 실행될 컴포넌트 
 
 const Contact = ()=>{
   return (
@@ -28,9 +29,18 @@ const App = ()=>{
   return (
     <div className="App">
       <h1>React Router DOM</h1>
-      <Home/>
-      <Tasks/>
-      <Contact/>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Tasks">Tasks</Link></li>
+          <li><Link to="/Contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Tasks" element={<Tasks/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
